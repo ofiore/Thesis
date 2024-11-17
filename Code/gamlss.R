bc0a5036cec3ef0c600777aa36d25722a112f0cb
Jun Yan <1536656+jun-yan@users.noreply.github.com>
2024-11-17 07:47:31 -0500

    added extraction of random effect variance

--- Code/gamlss.R
+++ Code/gamlss.R
@@ -25,24 +25,44 @@ library("qqconf")
 
 ## log-normal
 ln0  <- gamlss(RxnTime ~ 1, data = dhm, family = LOGNO)
-ln1  <- gamlss(RxnTime ~ random(Venue), data = dhm, family = LOGNO)
+ln1a  <- gamlss(RxnTime ~ 1 + random(Venue), data = dhm, family = LOGNO)
 ln2  <- gamlss(RxnTime ~ random(Venue) + random(Heat), data = dhm, family = LOGNO)
 ln3  <- gamlss(RxnTime ~ random(Venue) + random(Heat), sigma.formula = ~ random(Venue), data = dhm, family = LOGNO)
 ln4  <- gamlss(RxnTime ~ random(Venue) + random(Heat), sigma.formula = ~ random(Venue) + random(Heat), data = dhm, family = LOGNO)
+
+AIC(ln0, ln1, ln2, ln3, ln4)
 
 qq_conf_plot(residuals(ln0))
 qq_conf_plot(residuals(ln1))
 qq_conf_plot(residuals(ln2))
 
 ## three-parameter: generalized gamma
+gg1  <- gamlss(RxnTime ~ random(Venue), data = dhm, family = GG)
 gg2  <- gamlss(RxnTime ~ random(Venue) + random(Heat), data = dhm, family = GG)
 gg3  <- gamlss(RxnTime ~ random(Venue) + random(Heat), sigma.formula = ~ random(Venue), data = dhm, family = GG, control = gamlss.control(n.cyc = 40))
+gg3a  <- gamlss(RxnTime ~ random(Venue) + random(Heat), sigma.formula = ~ random(Heat), data = dhm, family = GG, control = gamlss.control(n.cyc = 40))
+gg3b  <- gamlss(RxnTime ~ random(Venue), sigma.formula = ~ random(Heat), data = dhm, family = GG, control = gamlss.control(n.cyc = 40))
+gg3c  <- gamlss(RxnTime ~ random(Heat), sigma.formula = ~ random(Heat), data = dhm, family = GG, control = gamlss.control(n.cyc = 40))
 gg4  <- gamlss(RxnTime ~ random(Venue) + random(Heat), sigma.formula = ~ random(Venue) + random(Heat), data = dhm, family = GG, control = gamlss.control(n.cyc = 40))
 
-AIC(gg3)
-AIC(gg4)
-qq_conf_plot(residuals(gg4))             
 
+AIC(gg1, gg2, gg3, gg3a, gg3b, gg3c, gg4) # gg3b is the best
+qq_conf_plot(residuals(gg3b))
+             
+### random effects in mu
+gg3b$mu.coefSmo
+gg3b$mu.coefSmo[[1]]$sigb
+gg3b$sigma.coefSmo[[1]]$sigb
+
+### Owen, with the random effects sd and the parameters (mu, sigma, nu),
+### you can develop code to simulate from the model for threshold evaluation
+
+### The lognormal fit is also quite good, except on the right tail.
+### Since our focus is the left tail, it'd be interesting to simulate
+### from the lognromal too as a sensitivity check for the threshold evaluation.
+
+### random effects in sigma
+             
 ## gamma
 ga0  <- gamlss(RxnTime ~ 1, data = dhm, family = GA)
 ga1  <- gamlss(RxnTime ~ random(Venue), data = dhm, family = GA)
